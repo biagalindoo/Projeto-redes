d591f490c669756edc8dbc833b8e523a91bb7c61
biagalindoo <biagalindo4@gmail.com>
2025-05-04 16:08:31 -0300

    Update server.py
    2 entrega

--- server.py
+++ server.py
@@ -1,24 +1,61 @@
 import socket
 
+def calcular_checksum(payload):
+    """Calcula o checksum simples somando os valores ASCII dos caracteres."""
+    return sum(ord(c) for c in payload) % 256 #(pra nao passar de 1 byte )
+
 def iniciar_servidor():
-    socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket_servidor.bind(('localhost', 12345))
-    socket_servidor.listen(1)
-    print("Servidor aguardando conexão...")
+    servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    servidor.bind(('localhost', 12345))
+    servidor.listen(1)
+    print("Aguardando conexão do cliente...")
 
-    socket_cliente, endereco_cliente = socket_servidor.accept()
-    print(f"Conexão estabelecida com {endereco_cliente}")
+    cliente, endereco = servidor.accept()
+    print(f"Conectado com {endereco}")
 
-    dados = socket_cliente.recv(1024).decode()
-    print(f"Dados recebidos do cliente: {dados}")
+    handshake = cliente.recv(1024).decode()
+    print(f"[Handshake] -> {handshake}")
 
-    resposta = f"Modo: operação padrão, Tamanho máximo: {dados.split()[-2]} caracteres"
-    socket_cliente.send(resposta.encode())
+    max_caracteres = handshake.split()[-2]
+    confirmacao_inicial = f"Configuração recebida. Máximo: {max_caracteres} caracteres por pacote."
+    cliente.send(confirmacao_inicial.encode())
+    print("Handshake finalizado. Aguardando pacotes...\n")
 
-    print(f"Handshaking completo! Servidor enviado: {resposta}")
+    mensagem_final = ""
+    contador_pacote = 0
 
-    socket_cliente.close()
-    socket_servidor.close()
+    while True:
+        trecho = cliente.recv(1024).decode()
+        if trecho == "FIM":
+            break
 
-if __name__ == "_main_":
+        if not trecho.startswith('D'):
+            continue  
+
+        num_seq = int(trecho[1])
+        payload = trecho[2:-3]
+        checksum_recebido = int(trecho[-3:])
+
+        checksum_calculado = calcular_checksum(payload)
+
+        print(f"Pacote recebido:")
+        print(f"  Número de Sequência: {num_seq}")
+        print(f"  Payload: '{payload}'")
+        print(f"  Checksum recebido: {checksum_recebido}, Checksum calculado: {checksum_calculado}")
+
+        if checksum_calculado == checksum_recebido:
+            mensagem_final += payload
+            ack_msg = f"ACK {num_seq}"
+            cliente.send(ack_msg.encode())
+        else:
+            nak_msg = f"NAK {num_seq}"
+            cliente.send(nak_msg.encode())
+
+    print("\nMensagem completa recebida:")
+    print(mensagem_final)
+
+    cliente.close()
+    servidor.close()
+
+if _name_ == "_main_":
     iniciar_servidor()
